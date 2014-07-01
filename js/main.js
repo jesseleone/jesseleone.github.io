ebe7aacebf4ac1c9dd35e966a1461caffc075d49
Jesse Leone <jesseleone@gmail.com>
2014-06-30 20:29:39 -0400

    Nav added

--- js/main.js
+++ js/main.js
@@ -7,7 +7,33 @@ $('#containerOne .linkMore').click(function(){
 	scrollToAnchor('containerTwoAnchor');
 });
 
+
+
 $('#containerThree .linkMore').click(function(){
 	scrollToAnchor('containerFourAnchor');
+});
+
+
+//NavBar
+$('#portfolio').click(function(){
+	scrollToAnchor('containerTwoAnchor');
+});
+
+$('#about').click(function(){
+	scrollToAnchor('containerThreeAnchor');
+});
+
+$('#brands').click(function(){
+	scrollToAnchor('containerFourAnchor');
+});
+
+$('#contact').click(function(){
+	scrollToAnchor('containerFiveAnchor');
+});
+
+
+//Footer
+$('#backToTop a').click(function(){
+	scrollToAnchor('containerOneAnchor');
 });
 
